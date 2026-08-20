251b188a8aa2acda2325acd7c22f75714299f21a
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2026-08-20 18:31:17 +0100

    First use case : Type pattern using switch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Describe(object ob)
+{
+    return ob switch
+    {
+        int => "Integer",
+        string => "String",
+        bool => "Boolean",
+        null => "Null",
+        _ => "Other"
+    };
+}
+
+Console.WriteLine(Describe(new List<int>() { 1, 2}));
