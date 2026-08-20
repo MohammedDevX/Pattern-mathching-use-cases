ec32e67b5243cb4a20a82c6ca9af59b700ca5a10
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2026-08-20 19:25:57 +0100

    List Pattern use case

---
+++ Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_matching_use_cases
+{
+    internal class Payment
+    {
+    }
+}
